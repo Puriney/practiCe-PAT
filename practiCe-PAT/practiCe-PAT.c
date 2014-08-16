f3c7fd601b64d4ad87ef2029af7ebede1e108713
Puriney <youryanyun@gmail.com>
2014-08-17 01:00:53 +0800

    Two issues solved:
    - Makefile is implemented
    - Repository deployed well

--- practiCe-PAT/practiCe-PAT.c
+++ practiCe-PAT/practiCe-PAT.c
@@ -15,20 +15,23 @@
 static int usage()
 {
 	fprintf(stderr, "\n");
-	fprintf(stderr, "Usage:    practiCe <题目编号> <参数>\n");
-	fprintf(stderr, "题目编号:        02-0        整数的四则运算\n");
+	fprintf(stderr, "Usage: practiCe <题目编号> <参数>\n");
+	fprintf(stderr, "题目编号:\n");
+	fprintf(stderr, "02-0\t整数的四则运算\n");
 	return 1; // Running come across with certain problems.
 }
 
 int main(int argc, char const *argv[])
 {
+
 	if (argc == 1) return usage();
-	if (strcmp(argv[1], "02-0")) {
-        fprintf(stderr, "02-0 Puzzle\n");
+
+	if (strcmp(argv[1], "02-0") == 0) {
+//        fprintf(stderr, "%s 问题\n", argv[1]);
         ElementaryArithmetic();
     }
 	else {
-		fprintf(stderr, "[main] unrecognized commad '%s'. Abort!\n", argv[1]);
+		fprintf(stderr, "找不到['%s']题，请确认是否存在于题库中或者等待我更新\n", argv[1]);
 		return 1;
 	}
 	return 0;
