44e16ed05e51e00b8062536349657245221d5ef8
Puriney <youryanyun@gmail.com>
2014-08-17 21:29:36 +0800

    submit file under right directory in local machine

--- practiCe-PAT/ElementaryArithmetic.c
+++ practiCe-PAT/ElementaryArithmetic.c
@@ -3,6 +3,29 @@
 #include <unistd.h>
 #include "ElementaryArithmetic.h"
 
+//输入样例：
+//3 2
+//输出样例：
+//3 + 2 = 5
+//3 - 2 = 1
+//3 * 2 = 6
+//3 / 2 = 1
+
 void ElementaryArithmetic(void){
 	fprintf(stderr, "开始解决问题：整数四则运算\n");
+	
+	int a = 0;
+	int b = 0;
+	
+	int c_sum       = a + b;
+	int c_substract = a - b;
+	int c_product   = a * b;
+	int c_divide    = a / b;
+	
+	printf("%d + %d = %d\n", a, b, c_sum);
+	printf("%d - %d = %d\n", a, b, c_substract);
+	printf("%d * %d = %d\n", a, b, c_product);
+	printf("%d / %d = %d\n", a, b, c_divide);
+	
+	fprintf(stderr, "问题解决\n");
 }
