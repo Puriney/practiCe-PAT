196f7214f4c2198f8295cf958a247d4dbafe470c
Puriney <youryanyun@gmail.com>
2014-08-18 00:16:33 +0800

    愚蠢的PAT在线正确审查

--- practiCe-PAT/LengthConverter.c
+++ practiCe-PAT/LengthConverter.c
@@ -11,7 +11,7 @@
 #include "LengthConverter.h"
 
 void LengthConverter(void){
-	printf("// 输入格式：" \
+	printf("// 输入格式：\n" \
 		   "// 输入在一行中给出1个正整数，单位是厘米。\n");
 
 	//	公式1： foot = m / 0.3048
@@ -23,11 +23,9 @@ void LengthConverter(void){
 	//	输出样例：
 	//	5 6
 	int l_cm = 0;
-	int o_foot = 0;
-	int o_inch = 0;
 
 	float const EQUATION = 0.3048;
-	float const FOOTINCH = 12;
+	int const FOOTINCH = 12;
 	float const CM2M = 100.0;
 	printf("// 输入长度(单位厘米):\n");
 	scanf("%d", &l_cm);
@@ -41,8 +39,8 @@ void LengthConverter(void){
 	//	第二步
 	//	强制转换浮点型到整数型取整数foot
 	//	小数点转换为inch
-	o_foot = (int)m2foot;
-	o_inch = (int)((m2foot - o_foot) * FOOTINCH);
+	int o_foot = m2foot;
+	int o_inch = (m2foot - o_foot) * FOOTINCH;
 	
 	printf("%d %d\n", o_foot, o_inch);
 	
