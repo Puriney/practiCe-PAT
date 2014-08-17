2e6e2f4012de8dab5879c71ddfd0f1da4e0a315f
Puriney <youryanyun@gmail.com>
2014-08-17 22:32:03 +0800

    header file for LenghtConverter.c

--- practiCe-PAT/LengthConverter.h
+++ practiCe-PAT/LengthConverter.h
@@ -1,14 +1,1 @@
-//
-//  LengthConverter.h
-//  practiCe-PAT
-//
-//  Created by Yun Yan on 8/17/14.
-//  Copyright (c) 2014 Yun Yan. All rights reserved.
-//
-
-#ifndef practiCe_PAT_LengthConverter_h
-#define practiCe_PAT_LengthConverter_h
-
-
-
-#endif
+void LengthConverter(void); 
