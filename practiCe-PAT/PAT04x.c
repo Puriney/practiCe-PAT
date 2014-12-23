025b67944c4800eb1612f3a6e2222a4d3c0c51f6
Puriney <youryanyun@gmail.com>
2014-12-23 12:17:21 -0500

    PAT 04 finished.

---
+++ practiCe-PAT/PAT04x.c
@@ -0,0 +1,27 @@
+//
+//  PAT04x.c
+//  practiCe-PAT
+//
+//  Created by Yun Yan on 8/21/14.
+//  Copyright (c) 2014 Yun Yan. All rights reserved.
+//
+
+#include <stdio.h>
+#include "PAT04x.h"
+
+int PAT04x(char *puzzle){
+	int run = 0;
+	
+	if (strcmp(puzzle, "04-0") == 0) NumberCaller();
+	else if (strcmp(puzzle, "04-1") == 0) WaterLilies();
+	else if (strcmp(puzzle, "04-2") == 0) ProductBible();
+	else if (strcmp(puzzle, "04-3") == 0) SumOfPrimes();
+	else if (strcmp(puzzle, "04-4") == 0) GuessNum();
+	else {
+		run = 1;
+	}
+	
+	
+	
+	return run;
+}
---
+++ practiCe-PAT/PAT04x.h
@@ -0,0 +1,14 @@
+//
+//  PAT04x.h
+//  practiCe-PAT
+//
+//  Created by Yun Yan on 8/21/14.
+//  Copyright (c) 2014 Yun Yan. All rights reserved.
+//
+
+#ifndef practiCe_PAT_PAT04x_h
+#define practiCe_PAT_PAT04x_h
+
+
+
+#endif
